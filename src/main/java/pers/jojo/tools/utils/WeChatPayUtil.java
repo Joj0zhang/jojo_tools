33c25febeb47cbdb59a3dbc2d4ec5a54c67babbf
zhangchun <491812945@qq.com>
2020-03-10 14:57:57 +0800

    因MD5加密不满足collision resistance，故调整为SHA256加密

--- src/main/java/pers/jojo/tools/utils/WeChatPayUtil.java
+++ src/main/java/pers/jojo/tools/utils/WeChatPayUtil.java
@@ -1,5 +1,7 @@
 package pers.jojo.tools.utils;
 
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
 import java.security.MessageDigest;
 import java.security.SecureRandom;
 import java.util.*;
@@ -35,7 +37,8 @@ public class WeChatPayUtil {
         }
         sb.append("key=" + key);
         //System.out.println("签名字符串:" + sb.toString());
-        return string2MD5(sb.toString()).toUpperCase();
+//        return string2MD5(sb.toString()).toUpperCase();
+        return sha256_HMAC(sb.toString(),key).toUpperCase();
     }
 
     public static String string2MD5(String inStr) {
@@ -95,5 +98,43 @@ public class WeChatPayUtil {
     public static String createNonceStr() {
         SecureRandom random = new SecureRandom();
         return random.nextInt() + "";
+    }
+
+    /**
+     * sha256_HMAC加密
+     * @param message 消息
+     * @param secret  秘钥
+     * @return 加密后字符串
+     */
+    public static String sha256_HMAC(String message, String secret) {
+        String hash = "";
+        try {
+            Mac sha256_HMAC = Mac.getInstance("HmacSHA256");
+            SecretKeySpec secret_key = new SecretKeySpec(secret.getBytes(), "HmacSHA256");
+            sha256_HMAC.init(secret_key);
+            byte[] bytes = sha256_HMAC.doFinal(message.getBytes());
+            hash = byteArrayToHexString(bytes);
+        } catch (Exception e) {
+            System.out.println("Error HmacSHA256 ===========" + e.getMessage());
+        }
+        return hash;
+    }
+
+    /**
+     * 将加密后的字节数组转换成字符串
+     *
+     * @param b 字节数组
+     * @return 字符串
+     */
+    public  static String byteArrayToHexString(byte[] b) {
+        StringBuilder hs = new StringBuilder();
+        String stmp;
+        for (int n = 0; b!=null && n < b.length; n++) {
+            stmp = Integer.toHexString(b[n] & 0XFF);
+            if (stmp.length() == 1)
+                hs.append('0');
+            hs.append(stmp);
+        }
+        return hs.toString().toLowerCase();
     }
 }
